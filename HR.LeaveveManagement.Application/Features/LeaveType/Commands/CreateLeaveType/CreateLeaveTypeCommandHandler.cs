131eecf6af9781e3d980ec24ecd579125a42c741
SijuPunnedath <sijukodanad@gmail.com>
2023-08-03 20:39:03 +0530

    Create LeaveType Command completed

---
+++ HR.LeaveveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using HR.LeaveveManagement.Application.Contracts.Persistance;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HR.LeaveManagement;
+
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType
+{
+    public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, int>
+    {
+        private IMapper _mapper;
+        private ILeaveTypeRepository _leaveTypeRepository;   
+
+        public CreateLeaveTypeCommandHandler(IMapper mapper,ILeaveTypeRepository leaveTypeRepository)
+        {
+            this._mapper = mapper;   
+            this._leaveTypeRepository = leaveTypeRepository;
+             
+        }
+      
+
+        public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            //Validate INcoming Data
+
+            //Convert to domain entity object
+            var leaveTypeTocreate = _mapper.Map<Domain.LeaveType>(request);
+            //Add to Database
+           await _leaveTypeRepository.CreateAsync(leaveTypeTocreate);
+            //return recoed Id
+
+            return leaveTypeTocreate.Id;
+        }
+    }
+}
